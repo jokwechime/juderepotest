87ff97a0880c6e4d1979c1850dbe4f0bccf2f11f
jokwechime <72854596+jokwechime@users.noreply.github.com>
2023-04-14 11:20:31 +0100

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST:  'localhost',
-    USER:  'root',
-    PASSWORD: '!sioma@2004',
-    DB:  'ElectionDB',
+    HOST:  'us-cdbr-east-06.cleardb.net',
+    USER:  'bb8b34237224f6',
+    PASSWORD: 'fba043a9',
+    DB:  'heroku_1a6a2828e9d530a',
     dialect: 'mysql',
 
     pool: {
